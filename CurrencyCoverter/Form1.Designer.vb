bb36a04e46029f614854bc625b51ac88cc9df725
Project_Dreadman <romanpryce824@gmail.com>
2026-04-15 20:03:31 -0400

    Add project files.

---
+++ CurrencyCoverter/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.optSingapore = New System.Windows.Forms.RadioButton()
+        Me.optBrazil = New System.Windows.Forms.RadioButton()
+        Me.picFlag = New System.Windows.Forms.PictureBox()
+        Me.btnClac = New System.Windows.Forms.Button()
+        Me.txtUSD = New System.Windows.Forms.TextBox()
+        Me.lblFinal = New System.Windows.Forms.Label()
+        Me.optRussia = New System.Windows.Forms.RadioButton()
+        Me.optCuba = New System.Windows.Forms.RadioButton()
+        Me.optJapen = New System.Windows.Forms.RadioButton()
+        Me.optCanada = New System.Windows.Forms.RadioButton()
+        Me.optMexico = New System.Windows.Forms.RadioButton()
+        Me.optAstraila = New System.Windows.Forms.RadioButton()
+        Me.optIndia = New System.Windows.Forms.RadioButton()
+        Me.optEuro = New System.Windows.Forms.RadioButton()
+        CType(Me.picFlag, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'optSingapore
+        '
+        Me.optSingapore.AutoSize = True
+        Me.optSingapore.Location = New System.Drawing.Point(48, 44)
+        Me.optSingapore.Name = "optSingapore"
+        Me.optSingapore.Size = New System.Drawing.Size(91, 20)
+        Me.optSingapore.TabIndex = 0
+        Me.optSingapore.TabStop = True
+        Me.optSingapore.Text = "Singapore"
+        Me.optSingapore.UseVisualStyleBackColor = True
+        '
+        'optBrazil
+        '
+        Me.optBrazil.AutoSize = True
+        Me.optBrazil.Location = New System.Drawing.Point(48, 174)
+        Me.optBrazil.Name = "optBrazil"
+        Me.optBrazil.Size = New System.Drawing.Size(61, 20)
+        Me.optBrazil.TabIndex = 1
+        Me.optBrazil.TabStop = True
+        Me.optBrazil.Text = "Brazil"
+        Me.optBrazil.UseVisualStyleBackColor = True
+        '
+        'picFlag
+        '
+        Me.picFlag.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picFlag.Location = New System.Drawing.Point(199, 84)
+        Me.picFlag.Name = "picFlag"
+        Me.picFlag.Size = New System.Drawing.Size(401, 175)
+        Me.picFlag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picFlag.TabIndex = 2
+        Me.picFlag.TabStop = False
+        '
+        'btnClac
+        '
+        Me.btnClac.Location = New System.Drawing.Point(344, 41)
+        Me.btnClac.Name = "btnClac"
+        Me.btnClac.Size = New System.Drawing.Size(111, 23)
+        Me.btnClac.TabIndex = 3
+        Me.btnClac.Text = "Calculate"
+        Me.btnClac.UseVisualStyleBackColor = True
+        '
+        'txtUSD
+        '
+        Me.txtUSD.Location = New System.Drawing.Point(344, 338)
+        Me.txtUSD.Name = "txtUSD"
+        Me.txtUSD.Size = New System.Drawing.Size(100, 22)
+        Me.txtUSD.TabIndex = 4
+        '
+        'lblFinal
+        '
+        Me.lblFinal.AutoSize = True
+        Me.lblFinal.Location = New System.Drawing.Point(371, 296)
+        Me.lblFinal.Name = "lblFinal"
+        Me.lblFinal.Size = New System.Drawing.Size(38, 16)
+        Me.lblFinal.TabIndex = 5
+        Me.lblFinal.Text = "$0.00"
+        '
+        'optRussia
+        '
+        Me.optRussia.AutoSize = True
+        Me.optRussia.Location = New System.Drawing.Point(48, 278)
+        Me.optRussia.Name = "optRussia"
+        Me.optRussia.Size = New System.Drawing.Size(70, 20)
+        Me.optRussia.TabIndex = 6
+        Me.optRussia.TabStop = True
+        Me.optRussia.Text = "Russia"
+        Me.optRussia.UseVisualStyleBackColor = True
+        '
+        'optCuba
+        '
+        Me.optCuba.AutoSize = True
+        Me.optCuba.Location = New System.Drawing.Point(48, 252)
+        Me.optCuba.Name = "optCuba"
+        Me.optCuba.Size = New System.Drawing.Size(60, 20)
+        Me.optCuba.TabIndex = 7
+        Me.optCuba.TabStop = True
+        Me.optCuba.Text = "Cuba"
+        Me.optCuba.UseVisualStyleBackColor = True
+        '
+        'optJapen
+        '
+        Me.optJapen.AutoSize = True
+        Me.optJapen.Location = New System.Drawing.Point(48, 226)
+        Me.optJapen.Name = "optJapen"
+        Me.optJapen.Size = New System.Drawing.Size(66, 20)
+        Me.optJapen.TabIndex = 8
+        Me.optJapen.TabStop = True
+        Me.optJapen.Text = "Japen"
+        Me.optJapen.UseVisualStyleBackColor = True
+        '
+        'optCanada
+        '
+        Me.optCanada.AutoSize = True
+        Me.optCanada.Location = New System.Drawing.Point(48, 200)
+        Me.optCanada.Name = "optCanada"
+        Me.optCanada.Size = New System.Drawing.Size(76, 20)
+        Me.optCanada.TabIndex = 9
+        Me.optCanada.TabStop = True
+        Me.optCanada.Text = "Canada"
+        Me.optCanada.UseVisualStyleBackColor = True
+        '
+        'optMexico
+        '
+        Me.optMexico.AutoSize = True
+        Me.optMexico.Location = New System.Drawing.Point(48, 148)
+        Me.optMexico.Name = "optMexico"
+        Me.optMexico.Size = New System.Drawing.Size(71, 20)
+        Me.optMexico.TabIndex = 10
+        Me.optMexico.TabStop = True
+        Me.optMexico.Text = "Mexico"
+        Me.optMexico.UseVisualStyleBackColor = True
+        '
+        'optAstraila
+        '
+        Me.optAstraila.AutoSize = True
+        Me.optAstraila.Location = New System.Drawing.Point(48, 122)
+        Me.optAstraila.Name = "optAstraila"
+        Me.optAstraila.Size = New System.Drawing.Size(73, 20)
+        Me.optAstraila.TabIndex = 11
+        Me.optAstraila.TabStop = True
+        Me.optAstraila.Text = "Astraila"
+        Me.optAstraila.UseVisualStyleBackColor = True
+        '
+        'optIndia
+        '
+        Me.optIndia.AutoSize = True
+        Me.optIndia.Location = New System.Drawing.Point(48, 70)
+        Me.optIndia.Name = "optIndia"
+        Me.optIndia.Size = New System.Drawing.Size(57, 20)
+        Me.optIndia.TabIndex = 12
+        Me.optIndia.TabStop = True
+        Me.optIndia.Text = "India"
+        Me.optIndia.UseVisualStyleBackColor = True
+        '
+        'optEuro
+        '
+        Me.optEuro.AutoSize = True
+        Me.optEuro.Location = New System.Drawing.Point(48, 96)
+        Me.optEuro.Name = "optEuro"
+        Me.optEuro.Size = New System.Drawing.Size(56, 20)
+        Me.optEuro.TabIndex = 13
+        Me.optEuro.TabStop = True
+        Me.optEuro.Text = "Euro"
+        Me.optEuro.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.optEuro)
+        Me.Controls.Add(Me.optIndia)
+        Me.Controls.Add(Me.optAstraila)
+        Me.Controls.Add(Me.optMexico)
+        Me.Controls.Add(Me.optCanada)
+        Me.Controls.Add(Me.optJapen)
+        Me.Controls.Add(Me.optCuba)
+        Me.Controls.Add(Me.optRussia)
+        Me.Controls.Add(Me.lblFinal)
+        Me.Controls.Add(Me.txtUSD)
+        Me.Controls.Add(Me.btnClac)
+        Me.Controls.Add(Me.picFlag)
+        Me.Controls.Add(Me.optBrazil)
+        Me.Controls.Add(Me.optSingapore)
+        Me.Name = "Form1"
+        Me.Text = "Currency Coverter"
+        CType(Me.picFlag, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents optSingapore As RadioButton
+    Friend WithEvents optBrazil As RadioButton
+    Friend WithEvents picFlag As PictureBox
+    Friend WithEvents btnClac As Button
+    Friend WithEvents txtUSD As TextBox
+    Friend WithEvents lblFinal As Label
+    Friend WithEvents optRussia As RadioButton
+    Friend WithEvents optCuba As RadioButton
+    Friend WithEvents optJapen As RadioButton
+    Friend WithEvents optCanada As RadioButton
+    Friend WithEvents optMexico As RadioButton
+    Friend WithEvents optAstraila As RadioButton
+    Friend WithEvents optIndia As RadioButton
+    Friend WithEvents optEuro As RadioButton
+End Class
---
+++ CurrencyCoverter/Form1.vb
@@ -0,0 +1,163 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Application.Exit()
+    End Sub
+
+    Private Sub optSingapore_CheckedChanged(sender As Object, e As EventArgs) Handles optSingapore.CheckedChanged
+        If optSingapore.Checked = True Then
+            picFlag.Image = My.Resources.singapore
+        End If
+    End Sub
+
+    Function Singapore(ByVal USA As Decimal) As Decimal
+        Dim Conversion As Decimal
+        Conversion = USA * 1.29
+        Return Conversion
+    End Function
+
+    Private Sub btnClac_Click(sender As Object, e As EventArgs) Handles btnClac.Click
+        Dim USD As Decimal
+        Dim FinalAmount As String
+
+        'INPUT -->PROCESS -->OUTPUT
+        USD = txtUSD.Text
+
+        'PROCESS
+        If optSingapore.Checked = True Then
+            FinalAmount = "S$" & Singapore(USD)
+        ElseIf optBrazil.Checked = True Then
+            FinalAmount = "R$" & Brazil(USD)
+        ElseIf optEuro.Checked = True Then
+            FinalAmount = "€" & Euro(USD)
+        ElseIf optIndia.Checked = True Then
+            FinalAmount = "₹" & India(USD)
+        ElseIf optAstraila.Checked = True Then
+            FinalAmount = "A$" & Astraila(USD)
+        ElseIf optMexico.Checked = True Then
+            FinalAmount = "MXN$" & Mexico(USD)
+        ElseIf optCanada.Checked = True Then
+            FinalAmount = "C$" & Canada(USD)
+        ElseIf optJapen.Checked = True Then
+            FinalAmount = "¥" & Japen(USD)
+        ElseIf optCuba.Checked = True Then
+            FinalAmount = "₱" & Cuba(USD)
+        ElseIf optRussia.Checked = True Then
+            FinalAmount = "₽" & Russia(USD)
+        End If
+
+        'OUTPUT
+        lblFinal.Text = FinalAmount
+    End Sub
+
+    Private Sub optBrazil_CheckedChanged(sender As Object, e As EventArgs) Handles optBrazil.CheckedChanged
+        If optBrazil.Checked = True Then
+            picFlag.Image = My.Resources.Brazil
+        End If
+    End Sub
+
+    Function Brazil(ByVal usa As Decimal)
+        Dim Conversion As Decimal
+        Conversion = usa * 5.37
+        Return Conversion
+    End Function
+
+    Private Sub optEuro_CheckedChanged(sender As Object, e As EventArgs) Handles optEuro.CheckedChanged
+        If optEuro.Checked = True Then
+            picFlag.Image = My.Resources.euro
+        End If
+    End Sub
+
+    Function Euro(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 0.86
+        Return Conversion
+    End Function
+
+    Private Sub optIndia_CheckedChanged(sender As Object, e As EventArgs) Handles optIndia.CheckedChanged
+        If optIndia.Checked = True Then
+            picFlag.Image = My.Resources.india
+        End If
+    End Sub
+
+    Function India(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 90.14
+        Return Conversion
+    End Function
+
+    Private Sub optAstraila_CheckedChanged(sender As Object, e As EventArgs) Handles optAstraila.CheckedChanged
+        If optAstraila.Checked = True Then
+            picFlag.Image = My.Resources.australia
+        End If
+    End Sub
+
+    Function Astraila(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 1.56
+        Return Conversion
+    End Function
+
+    Private Sub optMexico_CheckedChanged(sender As Object, e As EventArgs) Handles optMexico.CheckedChanged
+        If optMexico.Checked Then
+            picFlag.Image = My.Resources.mexico
+        End If
+    End Sub
+
+    Function Mexico(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 18.0
+        Return Conversion
+    End Function
+
+    Private Sub optCanada_CheckedChanged(sender As Object, e As EventArgs) Handles optCanada.CheckedChanged
+        If optCanada.Checked = True Then
+            picFlag.Image = My.Resources.canada
+        End If
+    End Sub
+
+    Function Canada(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 1.39
+        Return Conversion
+    End Function
+
+    Private Sub optJapen_CheckedChanged(sender As Object, e As EventArgs) Handles optJapen.CheckedChanged
+        If optJapen.Checked = True Then
+            picFlag.Image = My.Resources.japan
+        End If
+    End Sub
+
+    Function Japen(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 157.61
+        Return Conversion
+    End Function
+
+    Private Sub optCuba_CheckedChanged(sender As Object, e As EventArgs) Handles optCuba.CheckedChanged
+        If optCuba.Checked = True Then
+            picFlag.Image = My.Resources.cuba
+        End If
+    End Sub
+
+    Function Cuba(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 24.0
+        Return Conversion
+    End Function
+
+    Private Sub optRussia_CheckedChanged(sender As Object, e As EventArgs) Handles optRussia.CheckedChanged
+        If optRussia.Checked = True Then
+            picFlag.Image = My.Resources.russia
+        End If
+    End Sub
+
+    Function Russia(ByVal USA As Decimal)
+        Dim Conversion As Decimal
+        Conversion = USA * 79.25
+        Return Conversion
+    End Function
+End Class
